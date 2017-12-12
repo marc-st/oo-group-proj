876fa6a7aa72fbac284b390238cdc95e15467984
marcstevens <marc.stevens99@gmail.com>
2017-12-12 13:01:17 +0000

    Initial Commit

---
+++ COM2038_Coursework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COM2038_Coursework
+//
+//  Created by Marc Stevens on 12/12/2017.
+//  Copyright © 2017 Marc Stevens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
